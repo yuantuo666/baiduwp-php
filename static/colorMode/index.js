7ae32715a29f80b1a3d29c1286d038daf8956e72
lc6464 <64722907+lc6464@users.noreply.github.com>
2021-02-08 17:00:56 +0800

    修复 static/colorMode/index.js 逻辑错误导致的自动配置错误纠正功能无法使用的问题。

--- static/colorMode/index.js
+++ static/colorMode/index.js
@@ -1,4 +1,4 @@
-let colorMode = localStorage.getItem('colorMode');
+const colorMode = localStorage.getItem('colorMode');
 if (colorMode === null) {
 	document.querySelector('#ColorMode-Auto').disabled = false;
 	if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -13,8 +13,15 @@ if (colorMode === null) {
 	document.querySelector('#ColorMode-Light').disabled = false;
 	document.querySelector('#Swal2-Light').disabled = false;
 } else {
-	Swal.fire('错误', '色彩模式配置出现错误，已重置配置！<br/>即将刷新页面……', 'error').then(function () {
+	if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		document.querySelector('#Swal2-Dark').disabled = false;
+	} else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+		document.querySelector('#Swal2-Light').disabled = false;
+	}
+	document.addEventListener('DOMContentLoaded', function () {
 		localStorage.removeItem('colorMode');
-		location.reload();
+		Swal.fire('错误', '色彩模式配置出现错误，已重置配置！<br/>即将刷新页面……', 'error').then(function () {
+			location.reload();
+		});
 	});
 }
