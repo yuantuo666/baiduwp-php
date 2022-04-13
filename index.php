cc7b10aa37b7e569298bfb1f81fae4954bda38b0
yuantuo666 <yuantuo666@gmail.com>
2022-04-13 12:53:08 +0800

    版本号更新

--- index.php
+++ index.php
@@ -9,14 +9,14 @@
  *
  * 此项目 GitHub 地址：https://github.com/yuantuo666/baiduwp-php
  *
- * @version 2.2.0
+ * @version 2.2.0.1
  *
  * @author Yuan_Tuo <yuantuo666@gmail.com>
  * @link https://imwcr.cn/
  * @link https://space.bilibili.com/88197958
  *
  */
-$programVersion_Index = "2.2.0";
+$programVersion_Index = "2.2.0.1";
 session_start();
 define('init', true);
 if (version_compare(PHP_VERSION, '7.0.0', '<')) {
